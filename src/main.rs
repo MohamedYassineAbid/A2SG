dbbb4d123b31e6d3503ceb28cce8644cefa9a320
MohamedYassineAbid <abidmohamedyassine@fss.u-sfax.tn>
2024-02-10 22:30:54 +0100

    updating the html file template

--- src/main.rs
+++ src/main.rs
@@ -19,7 +19,20 @@ struct PageDetails
     index :i32
 }
 fn main(){
-    let jStr = fs::read_to_string("map.json").expect("error loading json");
-    println!("{:?}",jStr);
-    let pageObj = serde_json::from_str::<Pages>(&jStr).expect("error structuring json ");
+    let j_str = fs::read_to_string("map.json").expect("error loading json");
+    let page_obj = serde_json::from_str::<Pages>(&j_str).expect("error structuring json ");
+    let mut template_str : String = "".to_string();
+    for e in 0..page_obj.pages.len()
+    {
+        let page_details = &page_obj.pages[e];
+        let temp = page_details.template;
+        if temp== true
+        {
+            let temp_path = &page_obj.pages[e].content;
+            template_str = fs::read_to_string(temp_path).expect("error loading html template");
+            break;
+        }
+
+    }
+    println!("{template_str:?}");
 }
